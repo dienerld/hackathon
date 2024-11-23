bbb1311e102bffddaa8968e76d28a80b9ead9f9d
anderwll <ander.wilmsen98@gmail.com>
2024-11-23 14:08:46 -0300

    refactor: add call in page app

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
-import { useSearchParams } from 'next/navigation'
+import { DialogRegister } from '@/components/DialogRegister'
 
 export default function App() {
-  const searchParams = useSearchParams()
-
-  const isFirstLogin = searchParams.get('first_login') === 'true'
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {isFirstLogin && <h1>Crie sua conta</h1>}
-      {!isFirstLogin && <h1>Bem vindo</h1>}
+
+      <DialogRegister />
     </div>
   )
 }
