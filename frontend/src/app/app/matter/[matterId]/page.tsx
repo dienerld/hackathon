c0b8f9fc40fe95621223b1273a6fcdb297a12bb2
anderwll <ander.wilmsen98@gmail.com>
2024-11-24 00:39:42 -0300

    refactor: adjust api url

--- frontend/src/app/app/matter/[matterId]/page.tsx
+++ frontend/src/app/app/matter/[matterId]/page.tsx
@@ -1,38 +1,37 @@
-import type { Topic } from "./entities/topic";
-import { useBreadcrumbs } from "@/contexts/breadcrumbs/useBreadcrumb";
-import { notFound } from "next/navigation";
-import { TopicCard } from "./components/TopicCard";
+import type { Topic } from './entities/topic'
+import { notFound } from 'next/navigation'
+import { TopicCard } from './components/TopicCard'
 
 interface MatterParams {
-  matterId: string;
+  matterId: string
 }
 
 export default async function Topics({
   params,
 }: {
-  params: Promise<MatterParams>;
+  params: Promise<MatterParams>
 }) {
-  const { matterId } = await params;
+  const { matterId } = await params
 
-  const filters = new URLSearchParams();
-  filters.append("matterId", matterId);
+  const filters = new URLSearchParams()
+  filters.append('matterId', matterId)
   const data = await fetch(
-    `http://localhost:8080/topics?${filters.toString()}`
-  );
+    `http://backend:8080/topics?${filters.toString()}`,
+  )
   if (!data.ok) {
-    notFound();
+    notFound()
   }
 
-  const topics = (await data.json()) as Topic[];
+  const topics = (await data.json()) as Topic[]
 
   return (
     <div className="flex flex-col justify-center">
       <h1 className="text-4xl font-bold text-center">Tópicos</h1>
       <ul className="list-none list-inside flex flex-col sm:flex-row gap-4 mt-4 ">
-        {topics.map((topic) => (
+        {topics.map(topic => (
           <TopicCard key={topic.id} topic={topic} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
