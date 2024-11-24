7b5eeb66ba86612e2a18454d91badad4ae01f85f
anderwll <ander.wilmsen98@gmail.com>
2024-11-24 02:03:16 -0300

    refactor: add quastions selects

--- frontend/src/app/app/matter/[matterId]/questions/[topicId]/page.tsx
+++ frontend/src/app/app/matter/[matterId]/questions/[topicId]/page.tsx
@@ -1,13 +1,53 @@
 'use client'
 
 import type { Question } from '../../entities/question'
+import { Button } from '@/components/ui/button'
+import { useToast } from '@/hooks/use-toast'
 import { getQuestionsByTopic } from '@/services/question.service'
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
+interface QuestionSelected {
+  questionId: string
+  selectedOption: {
+    label: string
+    value: string
+    correct: boolean
+  }
+}
+
 export default function Questions() {
   const { topicId } = useParams()
+  const { toast } = useToast()
+
+  const [currentQuestion, setCurrentQuestion] = useState(0)
   const [questions, setQuestions] = useState<Question[]>([])
+  const [optionSelected, setOptionSelected] = useState<QuestionSelected>()
+  const [optionsSelected, setOptionsSelected] = useState<QuestionSelected[]>([])
+
+  function handleSelectOption({ selectedOption }: Omit<QuestionSelected, 'questionId'>) {
+    setOptionSelected({ questionId: questions[currentQuestion].id, selectedOption })
+  }
+
+  function handleNextQuestion() {
+    if (!optionSelected)
+      return
+
+    // Temporario
+    if (optionSelected.selectedOption.correct) {
+      toast({
+        title: 'Resposta correta!',
+        description: 'Parabéns, você acertou!',
+      })
+    }
+    else {
+      toast({ title: 'Resposta incorreta!' })
+    }
+
+    setCurrentQuestion(currentQuestion + 1)
+    setOptionsSelected([...optionsSelected, optionSelected])
+    setOptionSelected(undefined)
+  }
 
   useEffect(() => {
     if (!topicId) {
@@ -20,13 +60,40 @@ export default function Questions() {
   }, [topicId])
 
   return (
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-1 h-full flex-col items-center">
       <h1 className="text-4xl font-bold text-center">Perguntas</h1>
-      <ul className="list-none list-inside flex flex-col sm:flex-row gap-4 mt-4 ">
-        {questions.map(question => (
-          <li key={question.id}>{question.name}</li>
-        ))}
-      </ul>
+
+      <div className="w-full h-full flex flex-col justify-center items-center flex-1">
+        {questions.length
+          ? (
+              <div>
+                <h2>{questions[currentQuestion].name}</h2>
+
+                <div className="flex flex-col gap-2 my-4">
+                  {questions[currentQuestion].options.map(option => (
+                    <div key={option.value}>
+                      <Button
+                        variant={optionSelected?.selectedOption.value === option.value ? 'default' : 'outline'}
+                        onClick={() => handleSelectOption({ selectedOption: option })}
+                      >
+                        {option.label}
+                      </Button>
+                    </div>
+                  ))}
+                </div>
+
+                <Button
+                  onClick={handleNextQuestion}
+                  disabled={currentQuestion === questions.length - 1 || !optionSelected}
+                >
+                  Proximo
+                </Button>
+              </div>
+            )
+          : (
+              <div>Loading...</div>
+            )}
+      </div>
     </div>
   )
 }
