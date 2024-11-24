a2ce08b48c66b6d33f4ae447a52d56312bf3c06d
anderwll <ander.wilmsen98@gmail.com>
2024-11-24 00:39:14 -0300

    refactor: add call get matters by classroom user logged

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,55 +1,41 @@
-"use client";
-import { MatterTypes } from "@/types";
-import { CardClass } from "@/components/CardClass";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
-import { DialogRegister } from "@/components/DialogRegister";
+'use client'
+
+import type { MatterTypes } from '@/types'
+import { CardClass } from '@/components/CardClass'
+import { DialogRegister } from '@/components/DialogRegister'
+import { getMattersByClassrom } from '@/services/matter.service'
+import { redirect } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [id, setId] = useState("");
-  const [alert, setAlert] = useState(false);
-  const [data, setData] = useState<MatterTypes[]>([
-    {
-      id: "",
-      name: "",
-      description: "",
-    },
-  ]);
-
-  useEffect(() => {
-    async function data() {
-      const response = await fetch(`http://localhost:8080/matters`);
-      const json = await response.json();
-
-      setData(json);
-    }
-
-    data();
-  }, []);
+  const user = JSON.parse(localStorage.getItem('user')!)
+  const [alert, setAlert] = useState(false)
+  const [data, setData] = useState<MatterTypes[]>([])
 
   function handleMatter(id: string) {
     if (!id) {
-      setAlert(!alert);
+      setAlert(!alert)
     }
-
-    redirect(`/app/matter/${id}`);
+    redirect(`/app/matter/${id}`)
   }
+
+  useEffect(() => {
+    if (!user)
+      return
+
+    getMattersByClassrom(user.classroomId).then((res) => {
+      if (res.error) {
+        return
+      }
+      setData(res.data)
+    })
+  }, [user])
+
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {id ? (
-        <Alert variant="destructive">
-          <AlertTitle>Erro</AlertTitle>
-          <AlertDescription>
-            Não foi possível encontrar o conteúdo
-          </AlertDescription>
-        </Alert>
-      ) : (
-        ""
-      )}
       <h1 className="font-semibold text-3xl">Disciplinas</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-2 gap-4">
-        {data.map((item) => (
+        {data.map(item => (
           <CardClass
             classe={item}
             key={item.id}
@@ -57,8 +43,7 @@ export default function App() {
           />
         ))}
       </div>
-      {/* <DialogRegister /> */}
       <DialogRegister />
     </div>
-  );
+  )
 }
