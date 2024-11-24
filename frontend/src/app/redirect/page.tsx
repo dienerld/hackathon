cec1105266a71ffe71dc74259b635fc4ad0368c2
anderwll <ander.wilmsen98@gmail.com>
2024-11-24 00:40:03 -0300

    refactor: adjust api url and  set user infos in localstorage

--- frontend/src/app/redirect/page.tsx
+++ frontend/src/app/redirect/page.tsx
@@ -1,45 +1,48 @@
-"use client";
+'use client'
 
-import { SplashScreen } from "@/components/SplashScreen";
-import { useUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { SplashScreen } from '@/components/SplashScreen'
+import { useUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
 export default function Redirect() {
-  const user = useUser();
+  const user = useUser()
 
   async function fetchData() {
-    const res = await fetch(`http://localhost:8080/users/${user.user?.id}`);
+    const res = await fetch(`http://localhost:8080/users/${user.user?.id}`)
 
-    const json = await res.json();
+    const json = await res.json()
     if (!res.ok) {
       return {
         error: json,
         data: null,
-      };
+      }
     }
     return {
       data: json,
       error: null,
-    };
+    }
   }
   useEffect(() => {
     if (!user.user) {
-      return;
+      return
     }
+
     fetchData().then((res) => {
       if (res.error) {
-        redirect("/app?first_login=true");
+        redirect('/app?first_login=true')
       }
-      redirect("/app");
-    });
-  }, [user.user]);
+
+      localStorage.setItem('user', JSON.stringify(res.data))
+      redirect('/app')
+    })
+  }, [user.user])
 
   return (
     <div className="flex flex-col justify-center items-center gap-10 py-24">
       <h1>Redirecting...</h1>
       <SplashScreen />
     </div>
-  );
+  )
 }
