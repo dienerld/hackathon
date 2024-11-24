8b787096a25e3b4827b3ae6adcb3b465e1f3647c
anderwll <ander.wilmsen98@gmail.com>
2024-11-24 00:38:19 -0300

    refactor: add returning fields user created

--- backend/src/modules/users/functions/create-user.ts
+++ backend/src/modules/users/functions/create-user.ts
@@ -1,13 +1,22 @@
+import z from 'zod'
 import { db, eq, tables } from '~/database'
 
 interface CreateUserRequest {
-
   firstName: string
   lastName: string
   email: string
   externalId: string
   classroomId: string
 }
+
+export const userResponseSchema = z.object({
+  id: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  classroomId: z.string().nullable(),
+  createdAt: z.date(),
+})
 
 export async function createUser({
   firstName,
@@ -16,22 +25,32 @@ export async function createUser({
   externalId,
   classroomId,
 }: CreateUserRequest) {
+  const returningFields = {
+    id: tables.user.id,
+    firstName: tables.user.firstName,
+    lastName: tables.user.lastName,
+    email: tables.user.email,
+    externalId: tables.user.externalId,
+    classroomId: tables.user.classroomId,
+    createdAt: tables.user.createdAt,
+  }
+
   const [userDb] = await db
-    .select({ id: tables.user.id })
+    .select(returningFields)
     .from(tables.user)
     .where(eq(tables.user.externalId, externalId))
     .limit(1)
     .execute()
 
   if (userDb) {
-    return { newUser: false, id: userDb.id }
+    return userResponseSchema.parse(userDb)
   }
 
   const [userCreated] = await db
     .insert(tables.user)
     .values({ firstName, lastName, email, externalId, classroomId })
-    .returning({ id: tables.user.id })
+    .returning(returningFields)
     .execute()
 
-  return { newUser: true, id: userCreated.id }
+  return userResponseSchema.parse(userCreated)
 }
