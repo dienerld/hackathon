26726aff74d60f5435bebfa827df7e19e2c50c14
anderwll <ander.wilmsen98@gmail.com>
2024-11-23 22:15:14 -0300

    feat: add call from register user and get all classroom

--- frontend/src/components/DialogRegister/index.tsx
+++ frontend/src/components/DialogRegister/index.tsx
@@ -1,29 +1,77 @@
 'use client'
 
+import { cn } from '@/lib/utils'
+import { getClassrooms } from '@/services/classroom.service'
+import { registerUser } from '@/services/user.service'
+import { useUser } from '@clerk/nextjs'
 import { useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button } from '../ui/button'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '../ui/dialog'
 import { Input } from '../ui/input'
-import { Progress } from '../ui/progress'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 
-const options = [
-  { value: 'apple', label: 'Apple' },
-  { value: 'apple1', label: 'Apple 1' },
-  { value: 'apple2', label: 'Apple 2' },
-  { value: 'apple3', label: 'Apple 3' },
-]
-
 export function DialogRegister() {
+  const { user } = useUser()
   const searchParams = useSearchParams()
   const isFirstLogin = searchParams.get('first_login') === 'true'
 
-  const [progress, setProgress] = useState(50)
+  const [options, setOptions] = useState<{ value: string, label: string }[]>([])
   const [form, setForm] = useState({
-    name: '',
-    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    classroomId: '',
   })
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const data = {
+      ...form,
+      externalId: user!.id,
+    }
+
+    registerUser(data).then((res) => {
+      console.log(res)
+    })
+  }
+
+  function handleChange(key: keyof typeof form) {
+    return (e: React.ChangeEvent<HTMLInputElement> | string) => {
+      const value = typeof e === 'string' ? e : e.target.value
+      setForm({
+        ...form,
+        [key]: value,
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (!user)
+      return
+
+    setForm({
+      ...form,
+      firstName: user.firstName!,
+      lastName: user.lastName!,
+      email: user.emailAddresses[0].emailAddress,
+    })
+  }, [user])
+
+  useEffect(() => {
+    getClassrooms().then((res) => {
+      if (res.error) {
+        console.error(res.error)
+        return
+      }
+
+      setOptions(res.data.map((classroom: any) => ({
+        value: classroom.id,
+        label: classroom.name,
+      })))
+    })
+  }, [])
 
   return (
     <Dialog
@@ -31,16 +79,22 @@ export function DialogRegister() {
     >
       <DialogContent className="flex flex-col gap-10 p-16 sm:max-w-md">
         <DialogHeader>
-          <DialogTitle className="text-center text-2xl">Gameficação Educacional</DialogTitle>
+          <DialogTitle className="text-center text-2xl">Bem Vindo</DialogTitle>
+          <DialogDescription
+            className="text-center"
+          >
+            Complete seu cadastro para continuar
+          </DialogDescription>
         </DialogHeader>
-        <div className="flex flex-col gap-4">
-
-          <Input id="firtsName" placeholder="Nome" />
-          <Input id="lastName" placeholder="Sobrenome" />
-          <Input id="email" placeholder="E-mail" />
-
-          {/* <Select>
-            <SelectTrigger className="w-full text-zinc-400">
+        <form
+          onSubmit={onSubmit}
+          className="flex flex-col gap-4"
+        >
+          <Input placeholder="Nome" value={form.firstName} onChange={handleChange('firstName')} />
+          <Input placeholder="Sobrenome" value={form.lastName} onChange={handleChange('lastName')} />
+          <Input placeholder="E-mail" disabled value={form.email} />
+          <Select value={form.classroomId} onValueChange={handleChange('classroomId')}>
+            <SelectTrigger className={cn('w-full', !form.classroomId && 'text-slate-500')}>
               <SelectValue placeholder="Turma" />
             </SelectTrigger>
             <SelectContent>
@@ -50,13 +104,16 @@ export function DialogRegister() {
                 ))}
               </SelectGroup>
             </SelectContent>
-          </Select> */}
+          </Select>
 
-          <Button type="submit" className="w-full">Continuar</Button>
-        </div>
-        <div className="w-full flex justify-center">
-          <Progress value={progress} className="w-[40%]" />
-        </div>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={!form.firstName || !form.lastName || !form.classroomId}
+          >
+            Continuar
+          </Button>
+        </form>
       </DialogContent>
     </Dialog>
   )
