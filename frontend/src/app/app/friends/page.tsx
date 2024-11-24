ca89cd943f922bd6b0c8df55bd76af93e3144a44
Diener Dornelas <diener.ld@outlook.com>
2024-11-24 12:58:54 -0300

    cria paginas para rotas em construção

---
+++ frontend/src/app/app/friends/page.tsx
@@ -0,0 +1,8 @@
+export default function RankingPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="font-semibold text-3xl">Amigos</h1>
+      <h2 className="font-semibold text-3xl">Em construção</h2>
+    </div>
+  )
+}
