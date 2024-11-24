1dbbc2be6c5aceefdec6ce537253d4ad76904331
anderwll <ander.wilmsen98@gmail.com>
2024-11-23 22:15:27 -0300

    refactor: remove use client

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,10 +1,8 @@
-'use client'
 import { DialogRegister } from '@/components/DialogRegister'
 
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-
       <DialogRegister />
     </div>
   )
